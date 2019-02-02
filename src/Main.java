38b1bd7b8b1ad9daf51f65a7a0ea5a424a936558
jj391 <844393369@qq.com>
2019-02-02 22:59:34 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+
+/**
+ * @author jj391
+ * @create 2019-02-02 22:34
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+}
